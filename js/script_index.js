9ccca4858a2643711d4f69759e26ac8c87bef86d
douglaslmts <douglongaray@gmail.com>
2021-01-26 18:34:41 -0300

    ajustando layout

--- js/script_index.js
+++ js/script_index.js
@@ -12,7 +12,7 @@ window.addEventListener('resize', function() { redimenciona() })
 
 function redimenciona() {
     x.style.margin = "150px auto";
-    x.style.backgroundColor = "silver";
+    x.style.backgroundColor = "";
     x.style.border = "";
     x.style.width = window.innerWidth + "px";
     x.style.height = window.innerHeight + "px";
@@ -23,38 +23,36 @@ function redimenciona() {
     x.style.height = window.innerHeight - 300 + "px";
     x.style.width = window.innerWidth - 100 + "px";
 
-    a.style.backgroundColor = "white";
-    a.style.marginTop = "50px";
-    a.style.border = "";
+    a.style.backgroundColor = "#3333ff";
+    a.style.marginTop = "400px";
+    a.style.border = "solid blue";
     a.style.textAlign = "le";
     a.style.width = "100%";
     a.style.height = "400px";
     a.style.borderRadius = "10px";
-    a.style.color = "black";
+    a.style.color = "white";
     a.style.fontSize = "200%";
     a.style.paddingLeft = "5%";
     a.style.paddingTop = "5%";
+    a.style.opacity = "90%";
 
     b.style.backgroundColor = "white";
     b.style.border = "";
     b.style.fontFamily = "font-family: Graphik-Medium, Graphik-Regular, 'Gotham SSm A', 'Gotham SSm B', 'Helvetica Neue', Helvetica, Arial, sans-serif";
     b.style.width = "100%";
-    b.style.marginTop = "50px";
     b.style.height = "100px";
     b.style.fontSize = "200%";
     b.style.color = "black";
     b.style.borderRadius = "10px";
     b.style.paddingLeft = "5%";
+    b.style.display = "inline-block";
 
-    c.style.backgroundColor = "white";
-    c.style.marginTop = "50px";
     c.style.border = "";
-    c.style.textAlign = "le";
-    c.style.width = "100%";
-    c.style.height = "400px";
+    c.style.textAlign = "center";
+    c.style.width = "30%";
+    c.style.height = "200px";
     c.style.borderRadius = "10px";
-    c.style.color = "black";
+    c.style.color = "#0D6EFD";
     c.style.fontSize = "200%";
-    c.style.paddingLeft = "5%";
-    c.style.paddingTop = "5%";
+
 }
